d60803c4802aa0d3345e8d63790720f6bc0dad62
Kay-ddggxh <kathrin.welfare@gmail.com>
2023-11-10 20:31:50 +0000

    create UserRegisterForm to customise sign up form

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+# source: https://stackoverflow.com/questions/62935406/how-to-make-a-signup-view-using-class-based-views-in-django#:~:text=from%20django%20import%20forms%0Afrom%20django.contrib.auth.models%20import%20User%0Afrom%20django.contrib.auth.forms%20import%20UserCreationForm%0A%0Aclass%20UserRegisterForm(UserCreationForm)%3A%0A%20%20email%20%3D%20forms.EmailField()%0A%0A%20%20class%20Meta%3A%0A%20%20%20%20%20%20model%20%3D%20User%0A%20%20%20%20%20%20fields%20%3D%20%5B%27username%27%2C%20%27email%27%2C%20%27first_name%27%5D  # noqa
+class UserRegisterForm(UserCreationForm):
+  email = forms.EmailField()
+
+  class Meta:
+      model = User
+      fields = ['username', 'email', 'first_name']
--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-# from django.views import generic
 from django.views.generic.edit import CreateView
+
+from .forms import UserRegisterForm
 
 # source: https://learndjango.com/tutorials/django-signup-tutorial#:~:text=from%20django.contrib.auth.forms%20import%20UserCreationForm%0Afrom%20django.urls%20import%20reverse_lazy%0Afrom%20django.views%20import%20generic%0A%0A%0Aclass%20SignUpView(generic.CreateView)%3A%0A%20%20%20%20form_class%20%3D%20UserCreationForm%0A%20%20%20%20success_url%20%3D%20reverse_lazy(%22login%22)%0A%20%20%20%20template_name%20%3D%20%22registration/signup.html%22  # noqa
 class SignUpView(SuccessMessageMixin, CreateView):
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     success_url = reverse_lazy("home")
     template_name = "users/signup.html"
     success_message = "Registration was successful"
