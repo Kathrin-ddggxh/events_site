bebdbcf471bee4fa444106a70d923745154682dc
Kay-ddggxh <kathrin.welfare@gmail.com>
2023-12-31 16:26:31 +0000

    add success message to add event view

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import UpcomingEvent
 from .forms import EventForm
@@ -12,16 +13,18 @@ class UpcomingEvents(ListView):
 
     template_name = "events/upcoming.html"
     model = UpcomingEvent
+    queryset = UpcomingEvent.objects.order_by('datetime')
     context_object_name = "events"
 
 
-class AddEvent(LoginRequiredMixin, CreateView):
+class AddEvent(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     """Add recipe view"""
 
     template_name = "events/add_event.html"
     model = UpcomingEvent
     form_class = EventForm
     success_url = "/events/"
+    success_message = "New event was added successfully"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
